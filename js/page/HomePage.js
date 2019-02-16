7fe929a7c1dbf4610c325752d91f0cf7886c8435
吕陈强 <lvchenqiang@ellabook.cn>
2019-02-16 16:04:31 +0800

    add anroid backhandler listen

--- js/page/HomePage.js
+++ js/page/HomePage.js
@@ -16,11 +16,48 @@ import {
   import NavigationUtil from '../navigator/NavigationUtil';
 
   import DynamicTabNavigator from '../navigator/DynamicTabNavigator';
+  import {NavigationActions} from "react-navigation";
+  import {BackHandler} from 'react-native';
+  import {connect} from 'react-redux';
+ class HomePage extends Component {
 
-export default class HomePage extends Component {
+componentDidMount(){
+BackHandler.addEventListener('hardwareBackPress',this.onBackPress);
+}
+
+componentWillUnmount(){
+BackHandler.removeEventListener('hardwareBackPress',this.onBackPress);
+}
+
+ /**
+     * 处理 Android 中的物理返回键
+     * https://reactnavigation.org/docs/en/redux-integration.html#handling-the-hardware-back-button-in-android
+     * @returns {boolean}
+     */
+    onBackPress = () => {
+      const {dispatch, nav} = this.props;
+      //if (nav.index === 0) {
+      if (nav.routes[1].index === 0) {//如果RootNavigator中的MainNavigator的index为0，则不处理返回事件
+          return false;
+      }
+      dispatch(NavigationActions.back());
+
+      return true;
+  };
+
+comdid
 
   render() {
     NavigationUtil.navigation = this.props.navigation
     return <DynamicTabNavigator/>
   }
 }
+
+
+const mapStateToProps = state => ({
+  nav: state.nav,
+});
+
+
+
+export default connect(mapStateToProps)(HomePage);
